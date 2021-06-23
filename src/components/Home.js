515471dbbd0991ee6799322f67cb38418043fc5e
eric santos <eric355402@yahoo.com>
2021-06-22 23:29:19 -0600

    fixed console warning

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import '../styles/home.css'
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
-
 import Navbar from './Navbar'
 
 const Home = () => {
