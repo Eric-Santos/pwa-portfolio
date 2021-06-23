8f9c3c2d31d2b6b5b7650df0c6470680d20affe1
eric santos <eric355402@yahoo.com>
2021-06-22 23:21:02 -0600

    added placeholder images on projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,27 @@
 import React from "react";
+import "../styles/projects.css";
+import Navbar from "./Navbar";
 
 const Projects = () => {
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className="projects">
+      <Navbar />
+      <h1>Projects Section</h1>
+      <p>
+        I have been focusing on Front-End Projects since I finished my
+        Full-Stack Development Course at Lambda School, and animation. Here some
+        of my latest projects
+      </p>
+      <section className="photos">
+        <img src="photos/placeholderFish.webp" alt="placeholder" />
+        <img src="photos/placeholderFish.webp" alt="placeholder" />
+        <img src="photos/placeholderFish.webp" alt="placeholder" />
+        <img src="photos/placeholderFish.webp" alt="placeholder" />
+        <img src="photos/placeholderFish.webp" alt="placeholder" />
+        <img src="photos/placeholderFish.webp" alt="placeholder" />
+        <img src="photos/placeholderFish.webp" alt="placeholder" />
+        <img src="photos/placeholderFish.webp" alt="placeholder" />
+      </section>
     </div>
   );
 };
