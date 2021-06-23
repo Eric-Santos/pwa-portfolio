05f144deae98db03c871d3bbb6420fd068b1e6d7
eric santos <eric355402@yahoo.com>
2021-06-22 21:06:34 -0600

    created navbar with conditional rendering

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,36 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+import "../styles/navbar.css";
+import { Link } from "react-router-dom";
+import { HiMenu } from "react-icons/hi";
 
 const Navbar = () => {
-  return (
-    <div>
-      <nav>
-         <Link to='/'>
-           <h1>Home</h1>
-         </Link>
-         <Link to='/projects'>
-           <h1>Projects</h1>
-         </Link>
-         <Link to='/about'>
-           <h1>About</h1>
-         </Link>
-         <Link to='/contact'>
-           <h1>Contact</h1>
-         </Link>
-      </nav>
-    </div>
-  );;
-}
+  const [active, setActive] = useState(false);;
 
-export default Navbar
+  const handleClick = () => {
+    setActive(!active);
+  }
+  
+  return (
+    <>
+      <div className="icon">
+        <HiMenu size="30" onClick={handleClick}        />
+      </div>
+      <nav className={active ? 'active' : null}>
+        <Link to="/" className="link">
+          <h1> Home </h1>
+        </Link>
+        <Link to="/projects" className="link">
+          <h1> Projects </h1>
+        </Link>
+        <Link to="/about" className="link">
+          <h1> About </h1>
+        </Link>
+        <Link to="/contact" className="link">
+          <h1> Contact </h1>
+        </Link>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
