4872093bcf1963f638eb79ace950ceef2c8dc2ec
eric santos <eric355402@yahoo.com>
2021-06-22 20:37:39 -0600

    added routes to navbar and basic components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const Home = lazy(() => import("./components/Home.js"));
+const About = lazy(() => import("./components/About.js"));
+const Projects = lazy(() => import("./components/Projects.js"));
+const Contact = lazy(() => import("./components/Contact.js"));
 
 
 function App() {
   return (
-   <Router>
-     <Suspense fallback={<div>Loading...</div>}>
-       <Switch>
-         <Route exact path='/' component={Home}/>
-       </Switch>
-     </Suspense>
-   </Router>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
-    return (
-        <div>
-            <nav><h1>NavBar</h1></nav>
-        </div>
-    )
+  return (
+    <div>
+      <nav>
+         <Link to='/'>
+           <h1>Home</h1>
+         </Link>
+         <Link to='/projects'>
+           <h1>Projects</h1>
+         </Link>
+         <Link to='/about'>
+           <h1>About</h1>
+         </Link>
+         <Link to='/contact'>
+           <h1>Contact</h1>
+         </Link>
+      </nav>
+    </div>
+  );;
 }
 
 export default Navbar
