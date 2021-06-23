1d6e54cba4be19b3d9417c8ee21f27b1bfc80918
eric santos <eric355402@yahoo.com>
2021-06-22 20:15:14 -0600

    created homepage, navbar and code splitting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const Home = lazy(() => import("./components/Home.js"));
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+     <Suspense fallback={<div>Loading...</div>}>
+       <Switch>
+         <Route exact path='/' component={Home}/>
+       </Switch>
+     </Suspense>
+   </Router>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav><h1>NavBar</h1></nav>
+        </div>
+    )
+}
+
+export default Navbar
