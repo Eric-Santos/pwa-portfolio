be34a74f54b3bbc193fab70f9dd60dc3d35fca0c
eric santos <eric355402@yahoo.com>
2021-07-06 11:40:03 -0600

    added lambda school profile icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,14 @@ import React from "react";
 import { HiOutlineMail } from "react-icons/hi";
 import { GoMarkGithub } from "react-icons/go";
 import { ImLinkedin } from "react-icons/im";
+import { CgProfile } from "react-icons/cg";
 import "../styles/footer.css";
 
 const Footer = () => {
   return (
     <>
       <footer>
-        <a
-          href="mailto:eric.santos.dev@gmail.com.com"
-        >
+        <a href="mailto:eric.santos.dev@gmail.com.com">
           {/* <HiOutlineMail size="30" /> */}
           eric.santos.dev@gmail.com
         </a>
@@ -22,8 +21,21 @@ const Footer = () => {
         >
           <GoMarkGithub size="25" />
         </a>
-        <a href="https://www.linkedin.com/in/ericsantos01/" alt="linkedin" rel="noopener noreferrer" target="_blank">
-          <ImLinkedin size="20" />
+        <a
+          href="https://www.linkedin.com/in/ericsantos01/"
+          alt="linkedin"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <ImLinkedin size="25" />
+        </a>
+        <a
+          href="profiles.lambdaschool.com/?g=recYeXiWWNoTe5sIL"
+          alt="lambda school profile"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <CgProfile size="25" />
         </a>
       </footer>
     </>
